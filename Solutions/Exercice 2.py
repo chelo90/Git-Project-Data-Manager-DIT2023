db372c90e7bfff0ec0890f05f611a83c93c8b699
chelo90 <sanoumarcellin@gmail.com>
2023-05-24 00:54:06 +0000

    Solution exercice 2

--- Solutions/Exercice 2.py
+++ Solutions/Exercice 2.py
@@ -1,3 +1,5 @@
+import turtle
+
 # Demander à l'utilisateur de saisir un nombre N
 L = float(input("longeur ? : "))
 l = float(input("largeur ? : "))
@@ -10,11 +12,10 @@ print("la surface est", S)
 P = (L+l)*2 
 print("le perimetre est", P)
 
-Q = bool(input("Voulez-vous tracer un rectangle ?"))
-if Q == True:
- {
+Q = bool(input("Voulez-vous tracer un rectangle ? :      "))
+if True :
   #Programme pour dessiner un rectangle avec le module turtle
-  import turtle
+  #Pour que le rectangle s'affiche un peu plus longtemps, utilisez des dimensions de L et l supérieures ou égale à 150
   tur = turtle.Turtle()
   tur.forward(L) # Déplace la tortue de L unités vers l'avant
   tur.left(90) # rotation de la tortue de 90 degrés
@@ -24,8 +25,7 @@ if Q == True:
   tur.left (90) # rotation de la tortue de 90 degrés
   tur.forward(l) # Déplace la tortue de l unités vers l'avant
   tur.left(90) # rotation de la tortue de 90 degrés
-  print("aurevoir")
- }
-
-else:
+  #print(tur)
+  print("aurevoir") 
+else :
   print("aurevoir")
